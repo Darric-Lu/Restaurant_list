56a2646ef82e449c2eb3e35a61a9a5730f7a3786
Darric <darric.lu@gmail.com>
2020-09-03 03:26:50 +0800

    Add update function

--- app.js
+++ app.js
@@ -45,11 +45,29 @@ app.post('/restaurants', (req, res) => {
 })
 
 app.get('/restaurants/:id', (req, res) => {
-  // console.log('req.params.id?', req.params.id)
   const id = req.params.id
   restaurantData.findById(id)
     .lean()
     .then(restaurant => res.render('show', { restaurant }))
+    .catch(error => console.error(error))
+})
+
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  restaurantData.findById(id)
+    .lean()
+    .then(restaurant => res.render('edit', { restaurant }))
+    .catch(error => console.error(error))
+})
+
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  restaurantData.findById(id)
+    .then(restaurant => {
+      Object.assign(restaurant, req.body)
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
     .catch(error => console.error(error))
 })
 
